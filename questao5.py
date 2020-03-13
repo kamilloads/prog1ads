bd8a0cb2b06d7a2948c1f2d49609edfa82319bd2
kamilloads <kamillogouveia@ads.fiponline.edu.br>
2020-03-13 19:04:24 -0300

    corrigi a questão

--- questao5.py
+++ questao5.py
@@ -3,5 +3,6 @@
 
 valor_o = float(input("Digite o valor original da dívida: "))
 dias_atraso = int(input("Digite a quantidade de dias em atrasado: "))
-valor_f = valor_o + dias_atraso * 0.25
+valor_dias = float(input("Digite o valor da multa por dias de atraso: "))
+valor_f = valor_o + dias_atraso * valor_dias
 print (f"O valor a ser pago é: {valor_f}")
